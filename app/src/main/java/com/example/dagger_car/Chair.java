829737347cce5c4177352b2c5bfa739bef01a2a7
armanziaei <armanziaei83@gmaio.com>
2021-03-28 12:37:20 +0430

    Feat Chair Class : chair class created for car

---
+++ app/src/main/java/com/example/dagger_car/Chair.java
@@ -0,0 +1,12 @@
+package com.example.dagger_car;
+
+public class Chair {
+
+    private int numberOfChairs;
+    private String size; //Like 2*5*6 meter;
+
+    public Chair(int numberOfChairs, String size) {
+        this.numberOfChairs = numberOfChairs;
+        this.size = size;
+    }
+}
