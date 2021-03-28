08530e60fa4ee3794fcca0895eedd552e7c3037b
armanziaei <armanziaei83@gmaio.com>
2021-03-28 12:32:53 +0430

    Feat Car Class : car class created

---
+++ app/src/main/java/com/example/dagger_car/Car.java
@@ -0,0 +1,16 @@
+package com.example.dagger_car;
+
+public class Car {
+
+    private Engine engine;
+    private Chair chair;
+
+    public Car(Engine engine, Chair chair) {
+        this.engine = engine;
+        this.chair = chair;
+    }
+
+    public void drive(){
+
+    }
+}
