00629468cedc412cd1ead5a62851f924261f42fe
armanziaei <armanziaei83@gmaio.com>
2021-03-28 12:35:17 +0430

    Feat Engine Class : engine class created for car class

---
+++ app/src/main/java/com/example/dagger_car/Engine.java
@@ -0,0 +1,14 @@
+package com.example.dagger_car;
+
+public class Engine {
+
+    private String speed;
+    private String cylinders;
+    private String brand;
+
+    public Engine(String speed, String cylinders, String brand) {
+        this.speed = speed;
+        this.cylinders = cylinders;
+        this.brand = brand;
+    }
+}
